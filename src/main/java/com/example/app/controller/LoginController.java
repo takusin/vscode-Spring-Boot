b590ff801ba7cbd95e2bfd1a0e20943b4c29371f
Mori <takumimori131@gmail.com>
2024-02-02 14:09:41 +0900

    ログインフォームの修正。及び、それに伴うLoginControllerのloginメソッドの修正

--- src/main/java/com/example/app/controller/LoginController.java
+++ src/main/java/com/example/app/controller/LoginController.java
@@ -1,18 +1,23 @@
 package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.app.domain.User;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("user", new User());
         return "login";
     }
 
     @PostMapping("/myPage")
-    public String myPage() {
+    public String processLogin(@ModelAttribute("user") User user) {
         return "myPage";
     }
 
