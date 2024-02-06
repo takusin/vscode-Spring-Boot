b58b2ab07ef632abebd86ebdb037a4c72bc41748
Mori <takumimori131@gmail.com>
2024-02-06 15:43:13 +0900

    DB連携のテスト実装の作成開始

--- src/main/java/com/example/app/controller/LoginController.java
+++ src/main/java/com/example/app/controller/LoginController.java
@@ -7,9 +7,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.app.domain.User;
+import com.example.app.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
+
+    private final UserService userService;
+
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("user", new User());
@@ -18,6 +25,7 @@ public class LoginController {
 
     @PostMapping("/myPage")
     public String processLogin(@ModelAttribute("user") User user) {
+
         return "myPage";
     }
 
---
+++ src/test/java/com/example/app/service/UserServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.app.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.app.mapper.PasswordMapper;
+import com.example.app.mapper.UsersMapper;
+
+@SpringBootTest
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UsersMapper usersMapper;
+
+    @Autowired
+    private PasswordMapper passwordMapper;
+
+    @Test
+    public void testUserIdAndPassword(){
+        //検索するユーザー名
+        String userName = "test8";
+
+        //ID検索
+        Integer userId = usersMapper.selectByName(userName);
+        assertNotNull(userId, "null : DBに存在しません");
+        System.out.println("result" + userId);
+
+        //パスワード検索
+        String password = passwordMapper.selectById(userId);
+        assertNotNull(password, "パスワードがnull : DBに存在しません");
+        System.out.println("result" + password);
+
+        //検証
+        assertTrue(password.length() > 0, "password is null");
+    }
+}
